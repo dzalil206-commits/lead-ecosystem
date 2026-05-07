8d03bd561dd69f251a2a3f0e4572fa7937e13ee8
dzalil206-commits <dzalil206@gmail.com>
2026-05-07 11:37:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -341,4 +341,8 @@ if __name__ == '__main__':
     with app.app_context():
         init_db()
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+@app.route('/blog/<slug>')
+def blog_post(slug):
+    return render_template(f'blog/{slug}.html')
     
